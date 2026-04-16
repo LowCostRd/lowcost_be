8f4b10be11605466b353d2324c1e1493bf1b503c
Divinegirl8 <iamoluchimercy@gmail.com>
2026-04-16 13:25:11 +0100

    added cors

--- app/routes/otp_verification.py
+++ app/routes/otp_verification.py
@@ -9,14 +9,14 @@ from ..constant.success_message import otp_verification_message
 email_verification_bp = Blueprint('email_verification', __name__)
 email_otp_service = EmailOTPService()
 
-@email_verification_bp.route("/verify_otp",methods=['POST'])
+@email_verification_bp.route("/v1/api/verify_otp",methods=['POST'])
 def verify_user_otp():
     data = request.get_json()
     email_otp_service.check_if_otp_is_verify_and_update_user_is_verified(data)
     json_response =  build_response(otp_verification_message,200)
     return jsonify(json_response),200
 
-@email_verification_bp.route("/resend_otp",methods=['PATCH'])
+@email_verification_bp.route("/v1/api/resend_otp",methods=['PATCH'])
 def resend_otp():
     data = request.get_json()
     response = email_otp_service.resend_otp(data)
