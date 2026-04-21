774949488d43f5b50d7207cab05dd14a79e345a6
Divinegirl8 <iamoluchimercy@gmail.com>
2026-04-21 16:57:34 +0100

    adjusted create account

--- app/models/practice_details.py
+++ app/models/practice_details.py
@@ -5,14 +5,15 @@ from app.models.enum.number_of_practitioners import NumberOfPractitioners
 
 
 class PracticeDetails:
-    def __init__(self,user_id,main_phone_number,website,number_of_practitioners: NumberOfPractitioners,insurance_plans:None):
-         self._id = str(uuid.uuid4())
+    def __init__(self,user_id,main_phone_number,website,number_of_practitioners: NumberOfPractitioners,insurance_plans:None, _id=None, created_at=None):
+         self._id = _id or str(uuid.uuid4())
          self.user_id = user_id
          self.main_phone_number = main_phone_number
          self.website = website
          self.number_of_practitioners = number_of_practitioners
          self.insurance_plans = insurance_plans or []
-         self.created_at = datetime.now()
+         self.created_at = created_at or datetime.now()
+         self.updated_at = datetime.now()
 
     
     def to_dict(self):
@@ -23,6 +24,7 @@ class PracticeDetails:
             "website": self.website,
             "number_of_practitioners": self.number_of_practitioners.value,
             "insurance_plans": self.insurance_plans,
-            "created_at": self.created_at
+            "created_at": self.created_at,
+            "updated_at": self.updated_at
 
         }
